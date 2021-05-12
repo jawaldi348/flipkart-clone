3380a1279a79558d3c8e876ebca8361cdb20d0e7
jawaldi348 <jawaldi416@gmail.com>
2021-05-12 15:11:42 +0700

    terima permintaan GET ke / dengan respons Hello from server

--- src/index.server.js
+++ src/index.server.js
@@ -5,6 +5,12 @@ const app = express();
 // environment variable or you can say constants
 env.config();
 
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Hello from server'
+    })
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
