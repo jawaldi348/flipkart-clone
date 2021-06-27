f2728a3211b63204c547069c6d36d6534ef9a72c
jawaldi348 <jawaldi416@gmail.com>
2021-06-27 14:05:21 +0700

    pindahkan proses simpan data user ke controller

---
+++ src/controller/user.js
@@ -0,0 +1,39 @@
+const User = require('../models/user');
+
+exports.signup = (req, res) => {
+    // periksa email ke database
+    User.findOne({ email: req.body.email })
+        // callback function
+        .exec((error, user) => {
+            // jika email sudah ada munculkan pesan
+            if (user) return res.status(400).json({
+                message: 'User already registered',
+                status: '1001'
+            });
+
+            const {
+                firstName, lastName, email, password
+            } = req.body;
+
+            const _user = new User({
+                firstName,
+                lastName,
+                email,
+                password,
+                username: Math.random().toString()
+            });
+
+            _user.save((error, data) => {
+                if (error) {
+                    return res.status(400).json({
+                        message: 'Something went wrong'
+                    });
+                }
+                if (data) {
+                    return res.status(201).json({
+                        user: data
+                    })
+                }
+            });
+        });
+}
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,47 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
 
 router.post('/signin', (req, res) => {
 
 });
 
 router.post('/signup', (req, res) => {
-    // periksa email ke database
-    User.findOne({ email: req.body.email })
-        // callback function
-        .exec((error, user) => {
-            // jika email sudah ada munculkan pesan
-            if (user) return res.status(400).json({
-                message: 'User already registered',
-                status: '1001'
-            });
 
-            const {
-                firstName, lastName, email, password
-            } = req.body;
-
-            const _user = new User({
-                firstName,
-                lastName,
-                email,
-                password,
-                username: Math.random().toString()
-            });
-
-            _user.save((error, data) => {
-                if (error) {
-                    return res.status(400).json({
-                        message: 'Something went wrong'
-                    });
-                }
-                if (data) {
-                    return res.status(201).json({
-                        user: data
-                    })
-                }
-            });
-        });
 });
 
 module.exports = router;
