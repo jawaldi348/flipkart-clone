2406689071c6f5319b1781ff6717bb0fc938c2c3
jawaldi348 <jawaldi416@gmail.com>
2021-06-27 14:07:22 +0700

    import controller user ke router user

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const { signup } = require('../controller/user');
 const router = express.Router();
 
 router.post('/signin', (req, res) => {
 
 });
 
-router.post('/signup', (req, res) => {
-
-});
+router.post('/signup', signup);
 
 module.exports = router;
