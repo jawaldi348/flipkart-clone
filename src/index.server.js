914bdc88c3972859752d21536a53739219e013f0
jawaldi348 <jawaldi416@gmail.com>
2021-06-27 13:08:58 +0700

    tambah profeix api

--- src/index.server.js
+++ src/index.server.js
@@ -36,6 +36,8 @@ app.post('/data', (req, res, next) => {
     })
 })
 
+app.use('/api', userRoutes);
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
