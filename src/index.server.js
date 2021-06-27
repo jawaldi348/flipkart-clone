227fe7b966e36521f316b1fe9e200c8e089bb050
jawaldi348 <jawaldi416@gmail.com>
2021-06-27 12:56:34 +0700

    koneksi mongodb dengan driver Node.js version 2.2.12 or later2.2.12 or later

--- src/index.server.js
+++ src/index.server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const env = require('dotenv');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // environment variable or you can say constants
 env.config();
+
+// Koneksi mongodb Driver: Node.js Version: 2.2.12 or later
+// mongodb://root:<password>@cluster0-shard-00-00.wio8y.mongodb.net:27017,cluster0-shard-00-01.wio8y.mongodb.net:27017,cluster0-shard-00-02.wio8y.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-udto7u-shard-0&authSource=admin&retryWrites=true&w=majority
+mongoose.connect(
+    `mongodb://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0-shard-00-00.wio8y.mongodb.net:27017,cluster0-shard-00-01.wio8y.mongodb.net:27017,cluster0-shard-00-02.wio8y.mongodb.net:27017/${process.env.MONGO_DB_DATABASE}?ssl=true&replicaSet=atlas-udto7u-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then(() => {
+    console.log('Database connected!');
+});
 
 app.use(bodyParser());
 
